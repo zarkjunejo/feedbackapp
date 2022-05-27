e2e349779c35956fd7c0a325f5cb257c2a17489f
zark junejo <57903629+zarkjunejo@users.noreply.github.com>
2022-05-27 19:10:54 +0500

    updated

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -69,6 +69,62 @@ function RatingSelect({ select }) {
                 />
                 <label htmlFor='num5'>5</label>
             </li>
+<li>
+                <input
+                    type='radio'
+                    id='num6'
+                    name='rating'
+                    value='6'
+                    onChange={handleChange}
+                    checked={selected === 6}
+                />
+                <label htmlFor='num6'>6</label>
+            </li>
+<li>
+                <input
+                    type='radio'
+                    id='num7'
+                    name='rating'
+                    value='7'
+                    onChange={handleChange}
+                    checked={selected === 7}
+                />
+                <label htmlFor='num7'>7</label>
+            </li>
+<li>
+                <input
+                    type='radio'
+                    id='num8'
+                    name='rating'
+                    value='8'
+                    onChange={handleChange}
+                    checked={selected === 8}
+                />
+                <label htmlFor='num8'>8</label>
+            </li>
+<li>
+                <input
+                    type='radio'
+                    id='num9'
+                    name='rating'
+                    value='9'
+                    onChange={handleChange}
+                    checked={selected === 9}
+                />
+                <label htmlFor='num9'>9</label>
+            </li>
+<li>
+                <input
+                    type='radio'
+                    id='num10'
+                    name='rating'
+                    value='10'
+                    onChange={handleChange}
+                    checked={selected === 10}
+                />
+                <label htmlFor='num10'>10</label>
+            </li>
+
 
         </ul>
     )
